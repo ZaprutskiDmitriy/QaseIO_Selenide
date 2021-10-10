32bf5c09d61c6f6eeaed58ea233afbbefa9a0765
Dmitriy Zaprutski <zaprutsciy@mail.ru>
2021-10-10 19:32:10 +0300

    Added TestCasePage, models, wrappers Modified CaseTest, ProjectTest

--- src/test/java/pages/ProjectDetailsPage.java
+++ src/test/java/pages/ProjectDetailsPage.java
@@ -1,13 +1,34 @@
 package pages;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class ProjectDetailsPage {
 
     public ProjectDetailsPage isOpened() {
-        $(".ms-2").shouldBe(Condition.visible);
+        $("img.avatar-xxs").shouldBe(Condition.visible);
         return this;
+    }
+
+    public void validateName(String projectName) {
+        $("p.header").shouldHave(Condition.exactText(projectName));
+    }
+
+    public TestCasePage clickNewTestCase() {
+
+        if($(".ms-2").text().contains("| 0 tests")) {
+            $(By.xpath("//a[text()='Create new case']")).click();
+        } else {
+            $("#create-case-button").click();
+        }
+        return new TestCasePage();
+    }
+
+    public void validateCaseName(String title) {
+        $$(".cases-container").shouldHave(CollectionCondition.texts(title));
     }
 }
--- src/test/java/tests/ProjectTest.java
+++ src/test/java/tests/ProjectTest.java
@@ -2,18 +2,15 @@ package tests;
 
 import org.testng.annotations.Test;
 import pages.LoginPage;
-import com.github.javafaker.Faker;
 
 public class ProjectTest extends BaseTest {
 
-//    static Faker faker;
-
     @Test
     public void projectShouldBeCreated() {
-//        faker = new Faker();
         new LoginPage()
                 .open()
                 .login("zaprutsciy@mail.ru", "fE25hPhVXiu8xiM")
-                .create("Test", " ", "Any description", "Private");
+                .create("Test", "", "Description", "Public")
+                .validateName("Test");
     }
 }
