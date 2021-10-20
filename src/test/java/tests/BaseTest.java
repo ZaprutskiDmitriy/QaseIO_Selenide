52e35ec61e75a4bd6c16f56018858e76e996859c
Dmitriy Zaprutski <zaprutsciy@mail.ru>
2021-10-20 20:39:15 +0300

    added Jenkins file

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -14,7 +14,6 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup() {
-
 //        Implicitly Wait
         Configuration.timeout = 10000;
         Configuration.pageLoadTimeout = 60000;
@@ -34,6 +33,21 @@ public class BaseTest {
 //        All clicks using JS
         Configuration.clickViaJs = false;
 
+/*        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        // ChromeDriver is just AWFUL because every version or two it breaks unless you pass cryptic arguments
+//AGRESSIVE: options.setPageLoadStrategy(PageLoadStrategy.NONE); // https://www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
+        options.addArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
+        options.addArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
+        options.addArguments("--headless"); // only if you are ACTUALLY running headless
+        options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+        options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+        options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+        options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+        options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+        WebDriver driver = new ChromeDriver(options);
+        setWebDriver(driver);
+*/
     }
 
     @AfterMethod
